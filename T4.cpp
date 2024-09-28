7ab1ce4cd8c953adf01f5bb5e18912f37a3a959b
samuel10iman <112088761+samuel10iman@users.noreply.github.com>
2024-09-28 11:54:52 -0500

    Update T4.cpp

--- T4.cpp
+++ T4.cpp
@@ -7,61 +7,65 @@ struct nodo {
 	nodo<T>* next;
 	T* top;
 	int tam;
-    nodo(nodo<T>* n = nullptr){
-        next = n;     
-        top=valor;
-		tam=0;
-    }
+	nodo(nodo<T>* n = nullptr) {
+		next = n;
+		top = valor;
+		tam = 0;
+	}
 };
 
 template <class T>
 struct ls {
-	nodo<T>* head=nullptr;
+	nodo<T>* head = nullptr;
 	void add(T v);
 	void del(T v);
-	bool find(T v, nodo<T>*& pos, T*&pos_v);
+	bool find(T v, nodo<T>*& pos, T*& pos_v);
 	void print();
 };
 
 template<class T>
 void ls<T>::add(T v) {
-	nodo<T>*p;
+	nodo<T>* p;
 	T* pv;
-	if(!find(v,p,pv)){
-		cout << "insertar " << v << endl;
-		if(p==nullptr){
-			head=new nodo<T>(head);
-			*(head->valor)=v;
+	if (!find(v, p, pv)) {
+		//cout << "insertar " << v << endl;
+		if (p == nullptr) {
+			head = new nodo<T>(head);
+			*(head->valor) = v;
 			head->tam++;
-		}else{
-			T vop=*p->top;
-			while(p){
-				T*tmp=p->top;
-				while(tmp>=pv){
-					*(tmp+1)=*tmp;
+		}
+		else {
+			T vop = *p->top;
+			while (p) {
+				T* tmp = p->top;
+				while (tmp >= pv) {
+					*(tmp + 1) = *tmp;
 					tmp--;
 				}
-				*pv=v;
-				if(p->tam==5){
-					if(!p->next){
-						p->next=new nodo<T>(p->next);
-						*(p->next->valor)=vop;
+				*pv = v;
+				if (p->tam == 5) {
+					if (!p->next) {
+						p->next = new nodo<T>(p->next);
+						*(p->next->valor) = v;
 						p->next->tam++;
-						p=p->next;
+						p = p->next;
 						return;
-					}else{
-						v=vop;
-						p=p->next;
-						pv=p->valor;
 					}
-				}else{
+					else {
+						v = vop;
+						p = p->next;
+						pv = p->valor;
+					}
+				}
+				else {
 					p->top++;
 					p->tam++;
-					p=p->next;
+					p = p->next;
 				}
 			}
 		}
-	}else{
+	}
+	else {
 		cout << v << " repetido" << endl;
 	}
 }
@@ -69,56 +73,60 @@ void ls<T>::add(T v) {
 
 template<class T>
 void ls<T>::del(T v) {
-	nodo<T>*p;
+	nodo<T>* p;
 	T* pv;
-	if(find(v,p,pv)){
-		cout << "eliminando " << v << endl;
+	if (find(v, p, pv)) {
+		//cout << "eliminando " << v << endl;
 		T temp;
-		T* tmp=pv;
-		while(p){
-			while(tmp<p->top){
-				*tmp=*(tmp+1);
+		T* tmp = pv;
+		while (p) {
+			while (tmp < p->top) {
+				*tmp = *(tmp + 1);
 				tmp++;
 			}
-			if(!p->next){
+			if (!p->next) {
 				p->top--;
 				p->tam--;
 				return;
 			}
-			if(p->next->tam==1){
-				if(!p->next){
-					head=nullptr;
+			if (p->next->tam == 1) {
+				if (!p->next) {
+					head = nullptr;
 					delete p;
-				}else{
-					*p->top=*p->valor;
-					nodo<T>*q=p->next;
-					p->next=nullptr;
+				}
+				else {
+					*p->top = *p->next->valor;
+					nodo<T>* q = p->next;
+					p->next = nullptr;
 					delete q;
 				}
 				return;
 			}
-			*p->top=*p->next->valor;
-			tmp=p->next->valor;
-			p=p->next;
+			*p->top = *p->next->valor;
+			tmp = p->next->valor;
+			p = p->next;
 		}
-	}else{
+	}
+	else {
 		cout << "no existe elemento " << v << endl;
 	}
 }
 
 template<class T>
-bool ls<T>::find(T v, nodo<T>*&pos, T*&pos_v) {
-	pos=head;
-	while(pos){
-		pos_v=pos->valor;
-		for(;pos_v <= pos->top && v > *(pos_v);pos_v++);
-		if(v==*pos_v){
+bool ls<T>::find(T v, nodo<T>*& pos, T*& pos_v) {
+	pos = head;
+	while (pos) {
+		pos_v = pos->valor;
+		for (; pos_v <= pos->top && v > *(pos_v); pos_v++);
+		if (v == *pos_v) {
 			return true;
-		}else if(pos_v>pos->top){
-			if(!pos->next){
+		}
+		else if (pos_v > pos->top) {
+			if (!pos->next) {
 				return false;
-			}else{
-				pos=pos->next;
+			}
+			else {
+				pos = pos->next;
 				continue;
 			}
 		}
@@ -131,64 +139,53 @@ template<class T>
 void ls<T>::print() {
 	nodo<T>* p = head;
 	cout << "HEAD -> ";
-	while(p){
-        cout << "[ ";
-		T*q=p->valor;
-		if(p->valor==p->top && p->tam==0){
+	while (p) {
+		cout << "[ ";
+		T* q = p->valor;
+		if (p->valor == p->top && p->tam == 0) {
 			cout << "- - - - - ";
-		}else{
-			int cont=0;
-			for(;q<=p->top;q++,cont++){
+		}
+		else {
+			int cont = 0;
+			for (; q <= p->top; q++, cont++) {
 				cout << *q << " ";
 			}
-			while(cont<5){cout << "- ";cont++;}
+			while (cont < 5) { cout << "- "; cont++; }
 		}
-        p=p->next;
-        cout << "] -> ";
-    }
+		p = p->next;
+		cout << "] -> ";
+	}
 	cout << " NULL" << endl;
 }
 
 int main() {
 	ls<int>l1;
-	l1.print();
-    l1.add(9);
-	l1.print();
-	l1.add(8);
-	l1.print();
-	l1.add(3);
-	l1.print();
-	l1.add(6);
-	l1.print();
-	l1.add(0);
-	l1.print();
-	l1.add(4);
-	l1.print();
-	l1.add(-1);
+	l1.print();		
+	l1.add(1);
 	l1.print();
 	l1.add(2);
 	l1.print();
+	l1.add(3);
+	l1.print();
+	l1.add(4);
+	l1.print();
+	l1.add(5);
+	l1.print();
+	l1.add(6);
+	l1.print();
 	l1.add(7);
 	l1.print();
-	l1.add(10);
+	l1.add(8);
 	l1.print();
-	l1.del(10);
-	l1.print();
-	l1.del(7);
+	l1.add(9);
 	l1.print();
 	l1.del(2);
 	l1.print();
-	l1.del(0);
-	l1.print();
-	l1.del(3);
-	l1.print();
-	/*l1.del(8);
-	l1.print();
-	l1.del(9);
+	l1.del(4);
 	l1.print();
 	l1.del(6);
 	l1.print();
-	l1.del(3);
-	l1.print();*/
+	l1.del(8);
+	l1.print();
 	return 0;
 }
